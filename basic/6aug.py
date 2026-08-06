a7c75885b4d0c7a7597adb483a02da4c58be8b1a
dishant1123 <dishantshah1417@gmail.com>
2026-08-06 08:48:03 +0530

    ....

---
+++ basic/6aug.py
@@ -0,0 +1,90 @@
+'''print("my name is  dishant shah.")
+print('my name is varun.')
+print("sita")
+print("my best  friend is  bhavesh.") 
+print("live in  delhi.")
+'''
+# comments : 
+"""
+1.single line comments :  # 
+2.multi line comments  : """ """ or ''' ''' 
+"""
+
+# escape sequence character : 
+
+"""
+1. \n : new line    
+2. \t : tab 
+3. end = end of  line  remove white space 
+"""
+
+# print("princy\n\n")
+"""print("roshini")
+print("varun    \t                     dhavan")
+"""
+# princy verma
+'''print("princy",end="\n\n")
+print("varma")
+'''
+
+# data type  :
+"""
+1. int : positive or negative value store
+2. float : decimal value store
+3. char/ str : a to z , name  , special character =====>@#$%^&*
+4. Boolean : True or False
+5. complex : i or  j ex : 23 +90j    1.immginary part : 90j  2. real:23 
+"""
+
+# variable declaration rules : 
+"""
+1. you can't start a variable name with number , special character.
+2. excepation  : _(underscore)
+3. you can use  alphabets , number in last 
+ex :
+a_ =90  # a_ variable   90 value 
+P =90   
+j12 = 90 
+"""
+
+# explanation  of  data type : 
+
+#1. int  : positive or negative value store
+
+"""a=2343454545656767867   # a variable  234 value ----> static value / fix  value 
+print(a)  # 234
+print("a value is :",a)
+print("a=",a)
+print(type(a))  # type ====> type casting 
+"""
+# 2.float : decimal value store
+"""b=454567894567.45674565656756756767678
+print(b)
+print("b value is :",b)
+print(type(b))
+"""
+#3. str /char : a to z , name  , special character =====>@#$%^&*
+"""
+c="piyush"
+print(c)
+print("c value is :",c)
+print(type(c))
+
+"""
+# 4. Boolean : True or False
+
+"""d=True   # True value is 1  False value is 0
+print(d)
+print("d value is :",d)
+print(type(d))
+"""
+
+# 5. complex : i or  j ex : 23 +90j   
+
+e=34+23j   # immignary part : 23j  real:34
+print(e)
+print("e value is :",e)
+print(type(e))
+
+f=20+10j 
+print(f+e)  # immignary part : 23j +10j  real:20+34
