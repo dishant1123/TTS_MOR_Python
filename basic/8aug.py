ab5428cc36f165ca788093c8d18a8a2d6633e5c2
dishant1123 <dishantshah1417@gmail.com>
2026-08-08 08:40:32 +0530

    ....

---
+++ basic/8aug.py
@@ -0,0 +1,113 @@
+# user  input int  number  : 
+
+"""
+a=int(input("enter value of  a :"))
+b=int(input("enter value of  b :"))
+
+print("addition of  two number is : ",a+b)
+# note : int  data type  can't store the float  value. 
+"""
+
+# float value  : 
+"""a=float(input("enter value of  a :"))
+b=float(input("enter value of  b :"))
+
+print("multiply of two number is  : ",a*b)
+
+# note : float  data type  can store the int  value. 
+"""
+
+# string  : 
+
+"""x=str(input("enter the  name  :"))
+y=input("enter the  sur-name  :")
+
+print("name  : ",x)
+print("sur-name  : ",y)
+print("full name :",x+" "+y)
+"""
+
+# convert data type  : 
+
+"""a=90 
+print(a)
+print(type(a))  # int type  
+
+# int  value convert into float 
+# b= float(a)
+# print(b)
+print("a value  convert in to float is :",float(a))  # 90 
+print("a value  convert in to str is :",str(a))  # "90"
+"""
+
+# operator  : 
+
+"""
+1. airthetical operator :  + - * / % //
+2. relational operator : == != > < >= <=
+3. logical operator : and or not
+4. member ship operator : in not in
+5. assignment operator :  += -= *= /= %= //=
+"""
+ 
+# a=int(input("enter value of  a :"))
+# b=int(input("enter value of  b :"))
+
+# print("addition of  two number is : ",a+b)
+# print("subtraction of  two number is : ",a-b)
+# print("multiplication of  two number is : ",a*b)
+# print("division of  two number is : ",a/b)   # 3.3333333333333335
+
+# print("modulas operator is  : ",a%b)
+# print("floor division of  two number is : ",a//b)  # give  int  
+
+x=90
+y=91
+
+"""print("greater than : ",x>y)
+print("less than : ",y<x)
+print("greater than or equal to : ",x>=y)
+print("less than or equal to : ",x<=y)
+print("equal to : ",x==y)
+print(" not  equal to : ",x!=y)
+"""
+
+# logical operator : and  , or 
+
+"""print("and  operator  : ", x>y and x!=y)
+print("or  operator  : ", x>y or x!=y)
+"""
+
+# list  : 
+
+"""l1 =[1,2,3,45,67]
+print(2 in l1)
+print(99 not in l1)
+"""
+# assignment  operator :
+
+"""a=10 
+b=20 
+# a =a +b    # a= a-b  --->a-=b  a =a *b ---> a*=b 
+a+=b
+print(a)
+"""
+
+# task  :1 
+"""
+ask user to enter the  int type  2 number and  concate them. 
+
+input  a =10 
+input  b =90 
+output : 1090 
+"""
+
+# task :2 
+"""
+ask user to enter the purchase value (int type) and  add GST at 18 % . asnwer will store in float type.
+
+input  a =50000 
+output : 59000.00 your total amt of purchase 
+
+"""
+
