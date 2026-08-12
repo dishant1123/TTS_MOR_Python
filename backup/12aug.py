b4c6bf9c027489fd327cd8556c265b5ffd9240e6
dishant1123 <dishantshah1417@gmail.com>
2026-08-12 16:26:21 +0530

    .....

---
+++ backup/12aug.py
@@ -0,0 +1,77 @@
+# print("Hello World!")
+
+"""
+print("smit thakker")
+print("live in ahmedabad")
+print("love to play cricket")
+print("i am a student")
+print("love pizza")
+"""
+# comment : 
+
+"""
+2 type  : 
+1. single line  comment : #   
+2. multi line comment   : ''' '''  or """  """ 
+"""
+
+# escape sequence character :
+
+"""
+1. \n : new line
+2. \t : tab
+3. end = end of  line  remove white space
+"""
+
+"""print("smit\n")
+print("thakker\n")
+print("live  ahmedabad \t\t love pizza")
+
+
+print("dishant",end="\t\t")
+print("shah")
+"""
+
+# data type :
+"""
+1. int         : positive or negative value store
+2. float       : decimal value store
+3. char/ str   : a to z , name  , special character =====>@#$%^&*
+4. boolean     : True or False
+5. complex     : i or  j  ex :23j  , 23+90j  ----> 2 part : real and immignary part
+"""
+
+# variable declaration rules :
+"""
+1. you can't start a variable name with number , special character.
+2. excepation  : _(underscore)
+3. you can use  alphabets , number in last
+"""
+
+a=9034567890456789456789  # a variable  90  static value  ----> fix value 
+print(a)
+print("a value is :",a)
+print("a =",a)
+print(type(a))  # type ----> function  --->data type  
+
+b=90.67 
+print(b)
+print("b value is :",b)
+print(type(b))
+
+c="smit"
+print(c)
+print("c value is :",c)
+print(type(c))
+
+d=True
+print(d)
+print("d value is :",d)
+print(type(d))
+
+e=90+12j 
+print(e)
+print("e value is :",e)
+print(type(e))
+g=10+10j
+print(g+e)
