5d92c527861661ad9f05f6e09814f19b6af7efb7
dishant1123 <dishantshah1417@gmail.com>
2026-08-14 17:25:59 +0530

    ....

---
+++ backup/14aug.py
@@ -0,0 +1,43 @@
+# user  input  for int : 
+
+"""
+a=int(input("enter the number a :"))
+b=int(input("enter the number b :"))
+
+print("a=",a)
+print("b=",b)
+print("sum of two number is  : ",a+b)
+"""
+# note : int can't store the  float value . 
+
+# user input for float :
+"""x=float(input("enter the number a :"))
+y=float(input("enter the number b :"))
+
+print("x=",x)
+print("y=",y)
+print("multiplication of two number is  : ",x*y)
+"""
+# note  : float can store the  integer value .
+
+# user input for string :
+
+"""a=str(input("enter the name:"))
+b=input("enter the sur-name:")
+
+print("name=",a)
+print("sur-name=",b)
+print(a+b)
+print(a+" "+b)
+print(a,end=" ")
+print(b)
+"""
+# convert :
+
+"""a=10 
+print("a value  is  integer :",a)
+print(type(a))
+
+print("a value  convert into float :",float(a))
+print("a value  convert into str :",str(a))
+"""
