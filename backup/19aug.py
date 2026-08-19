5946c2079b57708cd8053ca78340778ce406e6fb
dishant1123 <dishantshah1417@gmail.com>
2026-08-19 17:38:03 +0530

    .....

---
+++ backup/19aug.py
@@ -0,0 +1,46 @@
+# elif : 
+
+"""a=int(input("Enter the number a : "))
+b=int(input("Enter the number a : "))
+
+if a>b :
+    print("a is  big")
+elif b>a :
+    print("b is big")
+else :
+    print("a and b are equal")
+"""
+
+# ex :2 ask user to enter the  number and check  its divisible by 3 or  11 or both 
+"""
+num =int(input("Enter the number : "))
+
+if num % 11 ==0 and num % 3 ==0 :
+    print("The number is divisible by 3 and 11 both")
+elif num % 3==0:
+    print("The number is divisible by  3")
+elif num % 11==0 :
+    print("The number is divisible by 11")
+else :
+    print("not  divisible by 3 or 11")
+"""
+"""
+task :1 asl user to enter the  number and  check  its divisible by 5 or  11 or both
+task :2 ask user to enter the  cost price and sales price check whether its loss or profit or no profit no loss.
+
+hint   : profit = sale price - cost price
+        loss = sale price - cost price
+
+task :3 ask user to enter the 3 subjects marks  and calculate the  percentage and based on percentage  given  grade. 
+
+hint  : percentage = (mark/total mark) * 100
+
+percentage     grade
+90+            A+ 
+80-90          A
+70-80          B+
+60-70          B
+50-60          C+
+40-50          C
+less than 40   F
+"""
